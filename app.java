52b9f913a07f79cb687f80bd403d0970baa2f85c
oskar-047 <oscar213413@gmail.com>
2025-11-03 17:32:09 +0100

    Funcion avanzada para calcular conjetura de Collatz
    - Añadida una función para calcular la conjetura de Collatz de todos los números entre 2 y el número elegido por el usuario

--- app.java
+++ app.java
@@ -5,27 +5,72 @@ public class app{
 
 		// Definir variables
 		Scanner sc = new Scanner(System.in);
-		int initial_number;
 
-		System.out.println("Escribe un numero para empezar la conjetura de collatz: ");
+		System.out.println("Elige una opción: ");
+		System.out.println("1. Calcular conjetura de collatz de un número: ");
+		System.out.println("2. Calcular conjetura de collatz desde el número 2 hasta el número... : ");
 
 		while(true){
-			if(sc.hasNextInt()){
-				initial_number = sc.nextInt();
-				System.out.println("Seqüencia de Collatz para el número " + initial_number + ": ");
-				calculate(initial_number);
-				break;
-			} else{
-				System.out.println("Escribe un número valido: ");
-				// Consumir la entrada invalida
+			if(!sc.hasNextInt()){
+				System.out.println("Escribe un número correcto: ");
 				sc.nextLine();
+				continue;
+			}
+
+			int sel = sc.nextInt();
+			if(sel==1){
+				calculate_collatz(sc);
+				break;
+			} else if(sel==2){
+				calculate_several_collatz(sc);
+				break;
 			}
 		}
 
 		sc.close();
 	}
 
+	// Función que calcula la seqüencia de Collatz con 1 número
+	public static void calculate_collatz(Scanner sc){
+		while(true){
+			System.out.println("Escribe un número para calcular la conjetura de Collatz: ");
+			if(!sc.hasNextInt()){
+				sc.nextLine();
+				continue;
+			}
+
+			int number = sc.nextInt();
+			calculate(number);
+			break;
+		}
+	}
+
+	// Función que calcula toda la seqüencia de Collatz desde el número 2 hasta el que el usuario elija
+	public static void calculate_several_collatz(Scanner sc){
+		System.out.println(); // Linea vacia para legibilidad
+		System.out.println("Se va a calcular la conjetura de Collatz con los numeros entre 2 y ...: ");
+
+		while(true){
+			System.out.println("Escribe un numero valido (mayor que 2): ");
+			if(!sc.hasNextInt()){
+				sc.nextLine();
+				continue;
+			}
+
+			int number = sc.nextInt();
+			for(int i=2; i<number; i++){
+				System.out.println();
+				calculate(i);
+			}
+			break;
+		}
+	}
+
+	// Función que recibe 1 número como parametro y calcula su seqüencia de Collatz
 	public static void calculate(int number){
+		System.out.println("Seqüencia de collatz para el número: " + number);
+		String text = "";
+		int pasos = 0;
 		while(number!=1){
 			if(number%2!=0){
 				// Si es impar
@@ -34,7 +79,10 @@ public class app{
 				// si no lo es
 				number/=2;
 			}
-			System.out.println(number);
+			text += " " + number;
+			pasos++;
 		}
+		System.out.println("Seqüencia: " + text);
+		System.out.println("Pasos totales: " + pasos);
 	}
 }
